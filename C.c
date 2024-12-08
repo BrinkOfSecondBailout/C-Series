cb9b771daf2f1b2b5e82d669daea2e87e72b6506
Danny Nguyen <nguyenduongpa@gmail.com>
2024-12-07 18:32:10 -0800

    some code

---
+++ C.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+    // char line[1000];
+    // printf("Enter line\n");
+    // scanf("%[^\n]1000s", line);
+    // printf("Line: %s\n", line);
+
+    // char name[100];
+    // printf("Enter name\n");
+    // scanf("%100s", name);
+    // printf("Hello %s\n", name);
+
+    // int usf, euf;
+    // printf("Enter US Floor\n");
+    // scanf("%d", &usf);
+    // euf = usf - 1;
+    // printf("EU Floor %d\n", euf);
+
+    // printf("Hello world\n");
+    // printf("Answer %d\n", 42);
+    // printf("Name %s\n", "Sarah");
+    // printf("x %.1f i %d\n", 3.5, 100);
