2924b782c257c22041612ffbeacf4d38d070271c
Danny Nguyen <nguyenduongpa@gmail.com>
2024-12-09 14:23:35 -0800

    d

--- C.c
+++ C.c
@@ -3,7 +3,57 @@
 
 
 
+// int main() {
+//     // we use int instead of char for c in case it ends up being an EOF file
+//     int c;
+//     while ((c=getchar()) != EOF)
+//         putchar(c);
+// }
 
+
+// int main() {
+//     int c;
+//     c = getchar();
+
+//     while (c != EOF) {
+//         putchar(c);
+//         c = getchar();
+//     }
+// }
+
+
+// int main() {
+//     char c;
+//     c = getchar();
+//     printf("%c", c);
+// }
+
+
+
+// #define LOWER 0
+// #define UPPER 300
+// #define STEP 20
+
+// int main() {
+//     int fahr;
+//     for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+//         printf("%4d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
+// } 
+
+
+
+    // int lower, upper, step;
+    // float fahr, celsius;
+    // lower = 0;
+    // upper = 300;
+    // step = 20;
+    // fahr = lower;
+
+    // while (fahr <= upper) {
+    //     celsius = (5.0/9.0) * (fahr-32.0);
+    //     printf("%4.0f %6.1f\n", fahr, celsius);
+    //     fahr = fahr + step;
+    // }
 
 
 // int main() {
