97560c60b8850ea3a810d4ed1f4bd70ec66f7aca
Danny Nguyen <nguyenduongpa@gmail.com>
2024-12-11 15:06:37 -0800

    d

--- C.c
+++ C.c
@@ -1,28 +1,249 @@
 #include <stdio.h>
 
-#define YES 1
-#define NO 0
 
-int main() {
-    int c, nl, nw, nc, inword;
+// #define N 4
 
-    inword = NO;
-    nl = nw = nc = 0;
-    while ((c = getchar()) != EOF) {
-        ++nc;
-        if (c == '\n')
-            ++nl;
-        if (c == ' ' || c == '\n' || c == '\t')
-            inword = NO;
-        else if (inword == NO) {
-            inword = YES;
-            ++nw;
-        }
-    }
+// void detab();
 
-    printf("%d %d %d\n", nl, nw, nc);
-}
+// int main() {
+//     int c;
 
+//     while ((c = getchar()) != EOF) {
+//         if (c == '\t') {
+//             detab();
+//         } else {
+//             putchar(c);
+//         }
+//     }
+// }
+
+// void detab() {
+//     for (int i = 0; i < N; i++) {
+//         putchar(' ');
+//     }
+// }
+
+
+
+
+// #define MAXLINE 1000
+
+// char line[MAXLINE];
+// char save[MAXLINE];
+// int max;
+
+// void main() {
+//     int len;
+//     extern int max;
+//     extern char save[];
+//     int getline();
+//     max = 0;
+//     while ((len = getline()) > 0)
+//         if (len > max) {
+//             max = len;
+//             void copy();
+//         }
+//     if (max > 0)
+//         printf("%s", save);
+// }
+
+// int get_line() {
+//     int c, i;
+//     extern char line[];
+
+//     for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+//         line[i] = c;
+//     if (c == '\n') {
+//         line[i] = c;
+//         i++;
+//     }
+//     line[i] = '\0';
+//     return(i);
+// }
+
+// void copy() {
+//     int i;
+//     extern char line[], save[];
+
+//     i = 0;
+//     while ((save[i] = line[i]) != '\0')
+//         i++;
+// }
+
+
+
+
+
+
+// #define MAXLINE 1000
+
+// int main() {
+//     int len;
+//     int max;
+//     char line[MAXLINE];
+//     char save[MAXLINE];
+//     int get_line();
+//     int copy();
+
+//     max = 0;
+
+//     while ((len = get_line(line, MAXLINE)) > 0)
+//         if (len > max) {
+//             max = len;
+//             copy(line, save);
+//         }
+//     if (max > 0)
+//         printf("%s", save);
+// }
+
+// int get_line(s, lim)
+// char s[];
+// int lim;
+// {
+//     int c, i;
+
+//     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+//         s[i] = c;
+//     if (c == '\n') {
+//         s[i] = c;
+//         i++;
+//     }
+//     s[i] = '\0';
+//     return(i);
+// }
+
+// int copy(s1, s2)
+// char s1[], s2[];
+// {
+//     int i;
+
+//     i = 0;
+//     while ((s2[i] = s1[i]) != '\0')
+//         i++;
+// }
+
+
+
+// int main() {
+//     int lowercase();
+//     int c;
+
+//     while ((c = getchar()) != EOF) {
+//         putchar(lowercase(c));
+//     }
+// }
+
+// int lowercase(c)
+// int c; {
+//     if (c >= 'A' && c <= 'Z')  {
+//         return c + ('a' - 'A');
+//     }
+//     return c;
+// }
+
+
+
+
+// int main() {
+//     int i;
+//     int power();
+
+//     for (i = 0; i < 10; i++)
+//         printf("%d %d %d\n", i, power(2, i), power(-3, i));
+// }
+
+// int power(x, n)
+// int x, n;
+// {
+//     int i, p;
+//     p = 1;
+//     for (i = 1; i <= n; i++)
+//         p = p * x;
+//     return (p);
+// }
+
+
+// #define IN 1
+// #define OUT 0
+// #define MAX_WORD_LENGTH 10
+
+// int main() {
+//     int c, state = OUT;
+//     int word_length = 0;
+//     int word_lengths[MAX_WORD_LENGTH + 1] = {0};
+
+//     while ((c = getchar()) != EOF) {
+//         if (c == ' ' || c == '\n' || c == '\t') {
+//             if (state == IN) {
+//                 state = OUT;
+//                 if (word_length > 0 && word_length <= MAX_WORD_LENGTH)
+//                     word_lengths[word_length]++;
+//                 word_length = 0;
+//             }
+//         } else {
+//             state = IN;
+//             word_length++;
+//         }
+//     }
+
+//     printf("\Horizontal Histogram:\n");
+//     for (int i = 1; i <= MAX_WORD_LENGTH; i++) {
+//         printf("%2d: ", i);
+//         for (int j = 0; j < word_lengths[i]; j++) {
+//             printf("#");
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #define YES 1
+// #define NO 0
+
+// int main() {
+//     int c, nl, nw, nc, inword;
+
+//     inword = NO;
+//     nl = nw = nc = 0;
+//     while ((c = getchar()) != EOF) {
+//         ++nc;
+//         if (c == '\n')
+//             ++nl;
+//         if (c == ' ' || c == '\n' || c == '\t')
+//             inword = NO;
+//         else if (inword == NO) {
+//             inword = YES;
+//             ++nw;
+//         }
+//     }
+
+//     printf("%d %d %d\n", nl, nw, nc);
+// }
+
+// int main() {
+//     int c, i, nwhite,nother;
+//     int ndigit[10];
+
+//     nwhite = nother = 0;
+//     for (i = 0; i < 10; i++)
+//         ndigit[i] = 0;
+    
+//     while ((c = getchar()) != EOF)
+//         if (c >= '0' && c <= '9')
+//             ++ndigit[c -'0'];
+//         else if (c == ' ' || c == '\n' || c == '\t')
+//             ++nwhite;
+//         else
+//             ++nother;
+
+//     printf("digits =");
+
+//     for (i = 0; i < 10; ++i)
+//         printf(" %d", ndigit[i]);
+
+//     printf("\nwhite space = %d, other = %d\n", nwhite, nother);
+// }
 
 
 // int main() {
