db4719bbdd62cddfbef942cb4313e899d30cbf54
Danny Nguyen <nguyenduongpa@gmail.com>
2024-12-18 16:35:38 -0800

    d

--- C.c
+++ C.c
@@ -1,5 +1,81 @@
 #include <stdio.h>
 
+#define MAXOP 20 /* max size of operand, operator */
+#define NUMBER '0' /* signal that number found */
+#define TOOBIG '9' /* signal that string is too big */
+#define MAXVAL 100 /* maximum depth of val stack */
+
+int sp = 0; /* stack pointer */
+double val[MAXVAL]; /* value stack */
+
+int clear()
+{
+    sp = 0;
+}
+
+double push(double f) /* push f onto value stack */
+{
+    if (sp < MAXVAL)
+        return(val[sp++] = f);
+    else {
+        printf("error: stack full\n");
+        clear();
+        return(0);
+    }
+}
+
+double pop() {
+    if (sp > 0)
+        return(val[--sp]);
+    else {
+        printf("error: stack empty\n");
+        clear();
+        return(0);
+    }
+}
+
+int main() {
+    int type;
+    char s[MAXOP];
+    double op2;
+
+    while ((type = getop(s, MAXOP)) != EOF)
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("zero divisor popped\n");
+                break;
+            case '=':
+                printf("\t%f\n", push(pop()));
+                break;
+            case 'c':
+                clear();
+                break;
+            case TOOBIG:
+                printf("%.20s ... is too long\n", s);
+                break;
+            default:
+                printf("unknown command %c\n", type);
+                break;
+        }
+}
+
 
 double atof(s) /* convert string to double */
 char s[];
@@ -22,11 +98,6 @@ char s[];
     }
     return(sign * val / power);
 }
-
-
-
-
-
 
 
 
