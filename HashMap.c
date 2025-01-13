5772a1ddee22b77c199a1c07f8958320c0526d61
Danny Nguyen <nguyenduongpa@gmail.com>
2025-01-13 12:14:41 -0800

    d

---
+++ HashMap.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+struct HashMapEntry
+{
+    char *key;
+    int value;
+    struct HashMapEntry *__prev;
+    struct HashMapEntry *__next;
+};
+
+struct HashMap
+{
+    int __buckets;
+    struct HashMapEntry *__heads[8];
+    struct HashMapEntry *__tails[8];
+    int __count;
+
+    void (*put)(struct HashMap *self, char *key, int value);
+    int (*get)(struct HashMap *self, char *key, int def);
+    int (*size)(struct HashMap *self);
+    void (*dump)(struct HashMap *self);
+    struct HashMapIter *(*iter)(struct HashMap *self);
+    void (*del)(struct HashMap *self);
+};
+
+struct HashMapIter
+{
+    int __bucket;
+    struct HashMap *__map;
+    struct HashMapEntry *__current;
+
+    struct HashMapEntry *(*next)(struct HashMapIter *self);
+    void (*del)(struct HashMapIter *self);
+};
+
+void __HashMapIter_del(struct HashMapIter *self)
+{
+    free((void *)self);
+}
+
+struct HashMapEntry *__HashMapIter_next(struct HashMapIter *self)
+{
+    struct HashMapEntry *retval;
+
+    while (self->__current == NULL)
+    {
+        self->__bucket++;
+        if (self->__bucket >= self->__map->__buckets)
+            return NULL;
+        self->__current = self->__map->__heads[self->__bucket];
+    }
+    retval = self->__current;
+    if (self->__current != NULL)
+        self->__current = self->__current->__next;
+    return retval;
+}
+
+struct HashMapIter *__HashMapIter_new(struct HashMap *map)
+{
+    struct HashMapIter *iter = malloc(sizeof(*iter));
+
+    iter->__map = map;
+    iter->__bucket = 0;
+    iter->__current = map->__heads[iter->__bucket];
+
+    iter->next = &__HashMapIter_next;
+    iter->del = &__HashMapIter_del;
+    return iter;
+}
+
+void __HashMap_del(struct HashMap *self)
+{
+    free((void *)self);
+}
+
+int __HashMap_size(struct HashMap *self)
+{
+    return self->__count;
+}
+
+int getBucket(char *str, int buckets)
+{
+    unsigned int hash = 123456;
+    printf("\nHashing %s\n", str);
+    if (str == NULL)
+        return 0;
+    for (; *str; str++)
+    {
+        hash = (hash << 3) ^ *str;
+        printf("%c 0x%08x %d\n", *str, hash, hash % buckets);
+    }
+    return hash % buckets;
+}
+
+struct HashMapEntry *__HashMap_find(struct HashMap *self, char *key, int bucket)
+{
+    struct HashMapEntry *cur;
+    if (self == NULL || key == NULL)
+        return NULL;
+    for (cur = self->__heads[bucket]; cur != NULL; cur = cur->__next)
+    {
+        if (strcmp(key, cur->key) == 0)
+            return cur;
+    }
+    return NULL;
+}
+
+int __HashMap_get(struct HashMap *self, char *key, int def)
+{
+    int bucket = getBucket(key, self->__buckets);
+    struct HashMapEntry *retval = __HashMap_find(self, key, bucket);
+    if (retval == NULL)
+        return def;
+    return retval->value;
+}
+
+void __HashMap_put(struct HashMap *self, char *key, int value)
+{
+    int bucket;
+    struct HashMapEntry *old, *new;
+    char *new_key;
+    bucket = getBucket(key, self->__buckets);
+
+    old = __HashMap_find(self, key, bucket);
+    if (old != NULL)
+    {
+        old->value = value;
+        self->__count++;
+        return;
+    }
+
+    new = malloc(sizeof(*new));
+    new->key = key;
+    new->value = value;
+    new->__next = NULL;
+
+    if (self->__heads[bucket] == NULL)
+        self->__heads[bucket] = new;
+    if (self->__tails[bucket] != NULL)
+        self->__tails[bucket]->__next = new;
+
+    new->__prev = self->__tails[bucket];
+    self->__tails[bucket] = new;
+    self->__count++;
+}
+
+void __HashMap_dump(struct HashMap *self)
+{
+    int i;
+    struct HashMapEntry *cur;
+
+    printf("\nObject HashMap@ %p count=%d buckets=%d\n", self, self->__count, self->__buckets);
+
+    for (i = 0; i < self->__buckets; i++)
+    {
+        for (cur = self->__heads[i]; cur != NULL; cur = cur->__next)
+        {
+            printf("%s=%d [%d]\n", cur->key, cur->value, i);
+        }
+    }
+}
+
+struct HashMap *HashMap_new()
+{
+    struct HashMap *p = malloc(sizeof(*p));
+
+    p->__buckets = 8;
+    for (int i = 0; i < p->__buckets; i++)
+    {
+        p->__heads[i] = NULL;
+        p->__tails[i] = NULL;
+    }
+    p->__count = 0;
+
+    p->put = &__HashMap_put;
+    p->get = &__HashMap_get;
+    p->size = &__HashMap_size;
+    p->dump = &__HashMap_dump;
+    p->iter = &__HashMapIter_new;
+    p->del = &__HashMap_del;
+
+    return p;
+}
+
+int main()
+{
+    struct HashMap *map = HashMap_new();
+    map->put(map, "Danny", 1);
+    map->put(map, "Lindsey", 2);
+    map->put(map, "Shawn", 3);
+    struct HashMapIter *iter = map->iter(map);
+    struct HashMapEntry *current = iter->next(iter);
+    while (current)
+    {
+        printf("%s=%d\n", current->key, current->value);
+        current = iter->next(iter);
+    }
+    map->dump(map);
+    iter->del(iter);
+    map->del(map);
+}
---
+++ TreeMap.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+struct TreeMap {
+    struct TreeMapEntry *__root;
+    int __count;
+
+    void (*put)(struct TreeMap *self, char *key, int value);
+    void (*dump)(struct TreeMapEntry *root, int depth);
+    void (*dump_inorder)(struct TreeMapEntry *root);
+    void (*dump_preorder)(struct TreeMapEntry *root);
+    void (*dump_postorder)(struct TreeMapEntry *root);
+    void (*del)(struct TreeMap *self);
+};
+
+struct TreeMapEntry {
+    char *key;
+    int value;
+    struct TreeMapEntry *__left;
+    struct TreeMapEntry *__right;
+};
+
+struct TreeMapEntry *TreeMapEntry_new(char *key, int value) {
+    struct TreeMapEntry *entry = malloc(sizeof(*entry));
+    char *new_key = malloc(strlen(key) + 1);
+    strcpy(new_key, key);
+    entry->key = new_key;
+    entry->value = value;
+    entry->__left = NULL;
+    entry->__right = NULL;
+    return entry;
+}
+
+void __TreeMap_dump(struct TreeMapEntry *root, int depth) {
+    if (root != NULL) {
+        for (int i = 0; i < depth; i++) {
+            printf("  ");
+        }
+        printf("%s=%d\n", root->key, root->value);
+
+        __TreeMap_dump(root->__left, depth + 1);
+        __TreeMap_dump(root->__right, depth + 1);
+    }
+}
+
+void __TreeMap_dump_inorder(struct TreeMapEntry *root) {
+    if (root != NULL) {
+        __TreeMap_dump_inorder(root->__left);
+        printf("%s=%d\n", root->key, root->value);
+        __TreeMap_dump_inorder(root->__right);
+    }
+}
+
+void __TreeMap_dump_preorder(struct TreeMapEntry *root) {
+    if (root != NULL) {
+        printf("%s=%d\n", root->key, root->value);
+        if (root->__left != NULL) {
+            __TreeMap_dump_preorder(root->__left);
+        }
+        if (root->__right != NULL) {
+            __TreeMap_dump_preorder(root->__right);
+        }
+    }
+}
+
+void __TreeMap_dump_postorder(struct TreeMapEntry *root) {
+    if (root != NULL) {
+        if (root->__left != NULL) {
+            __TreeMap_dump_postorder(root->__left);
+        }
+        if (root->__right != NULL) {
+            __TreeMap_dump_postorder(root->__right);
+        }
+        printf("%s=%d\n", root->key, root->value);
+    }
+}
+
+void __TreeMap_put(struct TreeMap *self, char *key, int value) {
+    struct TreeMapEntry *cur = self->__root;
+    struct TreeMapEntry *parent = NULL;
+    int cmp;
+
+    while(cur != NULL) {
+        parent = cur;
+        cmp = strcmp(key, cur->key);
+        if (cmp == 0) {
+            cur->value = value;
+            return;
+        }
+        if (cmp < 0) {
+            cur = cur->__left;
+        } else {
+            cur = cur->__right;
+        }
+    }
+    struct TreeMapEntry *new_entry = TreeMapEntry_new(key, value);
+
+    if (parent == NULL) {
+        self->__root = new_entry;
+    } else if (cmp < 0) {
+        parent->__left = new_entry;
+    } else {
+        parent->__right = new_entry;
+    }
+    self->__count++;
+}
+
+void __TreeMap_del(struct TreeMapEntry *root)
+{
+    if (root != NULL)
+    {
+        __TreeMap_del(root->__left);
+        __TreeMap_del(root->__right);
+        free(root->key);
+        free(root);
+    }
+}
+
+void TreeMap_del(struct TreeMap *self)
+{
+    __TreeMap_del(self->__root);
+    free(self);
+}
+
+struct TreeMap * TreeMap_new() {
+    struct TreeMap *tree = malloc(sizeof(*tree));
+    tree->__root = NULL;
+    tree->__count = 0;
+    tree->put = &__TreeMap_put;
+    tree->dump = &__TreeMap_dump;
+    tree->dump_inorder = &__TreeMap_dump_inorder;
+    tree->dump_preorder = &__TreeMap_dump_preorder;
+    tree->dump_postorder = &__TreeMap_dump_postorder;
+    tree->del = &TreeMap_del;
+    return tree;
+}
+
+
+int main() {
+    struct TreeMap *map = TreeMap_new();
+    
+    map->put(map, "h", 22);
+    map->put(map, "h", 42);
+    map->put(map, "d", 8);
+    map->put(map, "b", 123);
+    map->put(map, "f", 6);
+    map->put(map, "k", 9);
+    map->put(map, "m", 68);
+
+    map->dump(map->__root, 0);
+
+    // printf("\nInorder:\n");
+    // map->dump_inorder(map->__root);
+
+    // printf("\nPreorder:\n");
+    // map->dump_preorder(map->__root);
+
+    // printf("\nPostorder:\n");
+    // map->dump_postorder(map->__root);
+
+    map->del(map);
+}
+
